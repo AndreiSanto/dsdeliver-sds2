786f4940b7c7c50bdffab925f839d47df226e82c
Andrei <andrei.santo@hotmail.com>
2021-01-05 22:05:28 -0300

    Put/orders/{id}/delivered

--- backend/src/main/java/com/devesuperior/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/com/devesuperior/dsdeliver/controllers/OrderController.java
@@ -2,12 +2,13 @@ package com.devesuperior.dsdeliver.controllers;
 
 import java.util.List;
 
-import javax.persistence.criteria.Order;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +31,12 @@ public class OrderController {
 		dto= service.insert(dto);
 		return ResponseEntity.ok().body(dto);
 	}
+	@PutMapping("/{id}/delivered")
+	public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+		OrderDTO dto = service.setDeliver(id);
+		return ResponseEntity.ok().body(dto);
+		
+	}
+	
 
 }
