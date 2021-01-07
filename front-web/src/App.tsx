135d5aa2a3e7891b1040ef53b052cdde4613fdea
Andrei <andrei.santo@hotmail.com>
2021-01-06 21:17:44 -0300

    home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./NavBar";
-
+import Home from  "./Home";
 function App() {
   return (
-    <div>
-   <Navbar />    
-   </div>
+    <>
+   <Navbar /> 
+   <Home />
+   </>
   );
 }
 
